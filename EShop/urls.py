d54ece70f3d2549ee601a4d51226de041367f52a
RockaDev <83962580+RockaDev@users.noreply.github.com>
2022-07-24 01:36:33 +0200

    Commit

--- EShop/urls.py
+++ EShop/urls.py
@@ -28,8 +28,7 @@ urlpatterns = [
     path('shop/cart/', include('checkout.urls')),
     path('', include('orders.urls')),
 
-    url(r'^uploads/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
