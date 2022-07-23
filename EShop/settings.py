4eab769d3eb5176593822c46d5248e188745c3f2
RockaDev <83962580+RockaDev@users.noreply.github.com>
2022-07-23 20:36:37 +0200

    Update settings.py

--- EShop/settings.py
+++ EShop/settings.py
@@ -149,6 +149,8 @@ MEDIA_ROOT = '/uploads/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
 
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
