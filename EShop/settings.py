cd813f19fc59ad8d756dc52d94f69626f75f2ce3
RockaDev <83962580+RockaDev@users.noreply.github.com>
2022-07-23 20:47:34 +0200

    Commit

--- EShop/settings.py
+++ EShop/settings.py
@@ -16,7 +16,7 @@ import django_heroku
 import psycopg2
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+REPOSITORY_ROOT = os.path.dirname(BASE_DIR)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -144,12 +144,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 STATIC_URL = '/static/'
 MEDIA_ROOT = '/uploads/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
-
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
