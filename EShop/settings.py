4313cdec68a38581a6f57c00849fc7512de9dd33
RockaDev <83962580+RockaDev@users.noreply.github.com>
2022-07-24 02:26:16 +0200

    Update settings.py

--- EShop/settings.py
+++ EShop/settings.py
@@ -147,7 +147,7 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT = [os.path.join(BASE_DIR, 'media')]
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
