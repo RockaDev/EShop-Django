7ff07864ceb4b8bd36a5c970a89422a004289bd2
RockaDev <83962580+RockaDev@users.noreply.github.com>
2022-07-29 17:36:58 +0200

    Update settings.py

--- EShop/settings.py
+++ EShop/settings.py
@@ -165,4 +165,5 @@ AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_S3_SIGNATURE_VERSION = "s3v4"
 AWS_S3_REGION_NAME = 'eu-central-1'
+AWS_S3_ADDRESSING_STYLE = "virtual"
 #STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
