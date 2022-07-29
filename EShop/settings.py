13c0d5e92211e793099ce74e47195c2b0e52e210
RockaDev <83962580+RockaDev@users.noreply.github.com>
2022-07-29 17:05:08 +0200

    Commit

--- EShop/settings.py
+++ EShop/settings.py
@@ -163,3 +163,4 @@ AWS_STORAGE_BUCKET_NAME = 'liquid-eshop-bucket1'
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+#STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
