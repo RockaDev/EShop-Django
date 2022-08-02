e18b991d60804f3a675d1d8d5ecb54bd9bcab420
RockaDev <83962580+RockaDev@users.noreply.github.com>
2022-08-02 14:58:30 +0200

    Commit

--- checkout/forms.py
+++ checkout/forms.py
@@ -2,14 +2,14 @@ from shop.models import ShippingAddress
 from django import forms
 
 class CheckoutForm(forms.ModelForm):
-    address = forms.CharField(max_length=9999)
-    post_num = forms.CharField(max_length=9999)
-    country = forms.CharField(max_length=9999)
-    name = forms.CharField(max_length=9999)
-    surname = forms.CharField(max_length=9999)
-    tel_number = forms.IntegerField(label="Telefonne cislo")
-    dic = forms.CharField(max_length=9999,required=False)
-    email = forms.EmailField(max_length=9999)
+    address = forms.CharField(max_length=9999,label="Adresa")
+    post_num = forms.CharField(max_length=9999,label="PSČ")
+    country = forms.CharField(max_length=9999,label="Krajina")
+    name = forms.CharField(max_length=9999,label="Meno")
+    surname = forms.CharField(max_length=9999,label="Priezvisko")
+    tel_number = forms.IntegerField(label="Telefónne číslo")
+    dic = forms.CharField(max_length=9999,required=False,label="DIČ")
+    email = forms.EmailField(max_length=9999,label="Email")
     order_id = forms.CharField(widget = forms.HiddenInput(), required = False)
     product = forms.CharField(widget = forms.HiddenInput(), required = False)
     transaction_id = forms.CharField(widget = forms.HiddenInput(), required = False)
@@ -17,6 +17,19 @@ class CheckoutForm(forms.ModelForm):
     class Meta:
         model = ShippingAddress
         fields = "__all__"
+
+    def __init__(self, *args, **kwargs):
+        super(CheckoutForm, self).__init__(*args, **kwargs)
+        self.fields['address'].widget.attrs.update({'placeholder':'Vaša Adresa'})
+        self.fields['post_num'].widget.attrs['placeholder'] = 'Poštové smerovacie číslo'
+        self.fields['country'].widget.attrs['placeholder'] = 'Krajina'
+        self.fields['name'].widget.attrs['placeholder'] = 'Krstné Meno'
+        self.fields['surname'].widget.attrs['placeholder'] = 'Priezvisko'
+        self.fields['tel_number'].widget.attrs['placeholder'] = 'Telefónne číslo'
+        self.fields['dic'].widget.attrs['placeholder'] = 'DIČ | nepovinné'
+        self.fields['email'].widget.attrs['placeholder'] = 'Email Adresa'
+
+        
 
     def save(self, commit=True):
         self.instance.address=self.cleaned_data['address']
